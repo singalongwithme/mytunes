3dae26ed5d64cc1b765e72d15ddc4173ec0bcc89
singalongwithme <csinghavong@gmail.com>
2014-08-07 19:21:27 -0700

    initial commit

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,35 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', this.enqueue, this);
+    this.on('dequeue', this.dequeue, this);
+    this.on('ended', this.playNext, this);
+  },
+
+  enqueue: function(){
+    if(this.length === 1)
+      this.playFirst();
+  },
+
+  playFirst: function(){
+    this.at(0).play();
+  },
+
+  dequeue: function(song){
+    if(this.at(0) === song){
+      this.playNext();
+    } else {
+      this.remove(song);
+    }
+  },
+
+  playNext: function(){
+    this.shift();
+    if(this.length >= 1){
+      this.playFirst();
+    } else {
+      this.trigger('stop');
+    }
   }
 
 });
